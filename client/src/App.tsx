a5affd1ba39b035b2774c7c14c2ff6fb964cbc9e
jdhn2 <jdhn2@users.noreply.github.com>
2026-05-01 16:37:26 +0900

    style(client): 인트로 CTA 버튼 규격 보정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import './App.css'
+import './styles/intro-cta.css'
 
 function App() {
   return <RouterProvider router={router} />
