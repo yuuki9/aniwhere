f0a74e3fb1511feb13aad029fb4b7ba9b74516fd
jdhn2 <jdhn2@users.noreply.github.com>
2026-05-01 16:49:46 +0900

    fix(client): 인트로 리뷰 피드백 반영

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import './App.css'
-import './styles/intro-cta.css'
 
 function App() {
   return <RouterProvider router={router} />
